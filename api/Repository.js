bd98b1a51bd668b38c37c86fd7fb08b05a62f03c
William <wtpwinberg@gmail.com>
2019-09-04 23:45:07 -0600

    axios instance passed into plugin

---
+++ api/Repository.js
@@ -0,0 +1,21 @@
+export default ($axios) => (resource) => ({
+  index() {
+    return $axios.$get(`/${resource}`)
+  },
+
+  create(payload) {
+    return $axios.$post(`/${resource}`, payload)
+  },
+
+  show(id) {
+    return $axios.$get(`/${resource}/${id}`)
+  },
+
+  update(payload) {
+    return $axios.$put(`/${resource}`, payload)
+  },
+
+  delete(id) {
+    return $axios.$delete(`/${resource}/${id}`)
+  }
+})
---
+++ plugins/repository.js
@@ -0,0 +1,11 @@
+import createRepository from '~/api/Repository.js'
+
+export default (ctx, inject) => {
+  const repositoryWithAxios = createRepository(ctx.$axios)
+
+  const repositories = {
+    posts: repositoryWithAxios('posts'),
+    users: repositoryWithAxios('users')
+  }
+  console.log(repositories)
+}
