8e5cd17fa4371bbabaf4d701cdbf3db6b8acdc7e
William <wtpwinberg@gmail.com>
2019-09-14 23:58:04 -0600

    add new task page created

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,22 +2,26 @@
     <v-app>
         <v-navigation-drawer v-model="drawer" app clipped>
             <v-list dense>
-                <v-list-item @click="goHome">
-                    <v-list-item-action>
-                        <v-icon>mdi-view-dashboard</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>Dashboard</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
-                <v-list-item @click="goSettings">
-                    <v-list-item-action>
-                        <v-icon>mdi-settings</v-icon>
-                    </v-list-item-action>
-                    <v-list-item-content>
-                        <v-list-item-title>Settings</v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
+                <nuxt-link to="/settings">
+                    <v-list-item @click="goHome">
+                        <v-list-item-action>
+                            <v-icon>mdi-view-dashboard</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>Dashboard</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </nuxt-link>
+                <nuxt-link to="/settings">
+                    <v-list-item @click="goSettings">
+                        <v-list-item-action>
+                            <v-icon>mdi-settings</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>Settings</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </nuxt-link>
             </v-list>
         </v-navigation-drawer>
 
