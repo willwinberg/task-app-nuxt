b71f2c810bb36f2b4839a7fcb0c40941fd855550
William <wtpwinberg@gmail.com>
2019-09-18 22:27:06 -0600

    created profile edit page

---
+++ components/form/UserProvider.vue
@@ -0,0 +1,17 @@
+<script>
+const users = [
+    { id: 1, name: 'John Joe', description: 'Has good name' },
+    { id: 2, name: 'John Doe', description: 'Is mysterious' }
+]
+
+export default {
+    name: 'UserProvider',
+    provide: {
+        fetch: (id) => Promise.resolve(users.find((x) => x.id === id)),
+        post: (user) => users.push(user) && Promise.resolve({ success: true })
+    },
+    render() {
+        return this.$slots.default
+    }
+}
+</script>
