6280e0f1957fc1768695e3551af1a3ed3a26a8f1
William <wtpwinberg@gmail.com>
2019-10-04 15:15:13 -0600

    decided to keep taskView within taskcard since it uses all the same info

--- components/TaskCard.vue
+++ components/TaskCard.vue
@@ -19,18 +19,41 @@
                 </v-list-item-content>
             </v-list-item>
         </v-card>
-        <TaskView :props="taskView" />
+
+        <v-dialog v-model="taskView" max-width="600px">
+            <v-card outlined @click="taskView = true">
+                <v-list-item three-line>
+                    <v-list-item-content>
+                        <div class="overline mb-4" v-text="task.priority"></div>
+                        <v-list-item-title
+                            class="headline mb-1"
+                            v-text="task.title"
+                        ></v-list-item-title>
+                        <v-list-item-subtitle
+                            v-text="task.description"
+                        ></v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+
+                <v-card-actions>
+                    <EditTaskForm />
+                    <v-btn outlined text>delete</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </div>
 </template>
 
 <script>
 import EditTaskForm from './EditTaskForm'
-import TaskView from './TaskView'
+import PriorityChip from './chips/PriorityChip'
+import SiteChip from './chips/SiteChip'
 
 export default {
     components: {
         EditTaskForm,
-        TaskView
+        PriorityChip,
+        SiteChip
     },
     props: {
         task: Object
