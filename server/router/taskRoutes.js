4a677793ffb6d2912984807b5ac3d5f443b38c77
William <wtpwinberg@gmail.com>
2019-10-16 23:21:49 -0600

    removed dumb comment titles

--- server/router/taskRoutes.js
+++ server/router/taskRoutes.js
@@ -1,7 +1,6 @@
 // const express = require('express')
 // const passport = require('passport')
 const Task = require('../models/Task')
-// const router = express.Router()
 
 module.exports = function(path, router) {
     router
@@ -73,5 +72,3 @@ module.exports = function(path, router) {
                 })
         })
 }
-
-// module.exports = router
--- server/router/userRoutes.js
+++ server/router/userRoutes.js
@@ -1,43 +1,31 @@
-// const express = require('express')
 const User = require('../models/User')
-// const Board = require('../models/boardModel')
-// const Task = require('../models/taskModel')
 
 module.exports = function(path, router) {
     router.use(path, router)
-    /* GET ALL USERS */
     router.get('/', function(req, res, next) {
         User.find(function(err, products) {
             if (err) return next(err)
             res.json(products)
         })
     })
-
-    /* GET SINGLE USER BY ID */
     router.get('/:id', function(req, res, next) {
         User.findById(req.params.id, function(err, post) {
             if (err) return next(err)
             res.json(post)
         })
     })
-
-    /* ADD USER */
     router.post('/', function(req, res, next) {
         User.create(req.body, function(err, post) {
             if (err) return next(err)
             res.json(post)
         })
     })
-
-    /* UPDATE USER */
     router.put('/:id', function(req, res, next) {
         User.findByIdAndUpdate(req.params.id, req.body, function(err, post) {
             if (err) return next(err)
             res.json(post)
         })
     })
-
-    /* DELETE USER */
     router.delete('/:id', function(req, res, next) {
         User.findByIdAndRemove(req.params.id, req.body, function(err, post) {
             if (err) return next(err)
