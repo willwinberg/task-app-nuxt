a0922599e73d348229118446c072151b64c6e39b
William <wtpwinberg@gmail.com>
2019-10-30 21:31:42 -0600

    fixed wrongly names auth routes reference

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-router.use('/auth', require('./authRoutes').router)
+router.use('/auth', require('./auth').router)
 router.use('/users', require('./user').router)
 router.use('/tasks', require('./task').router)
 
