e897fa71e314e272fc74c91cbb5edb17d670ede1
William <wtpwinberg@gmail.com>
2020-03-06 17:02:19 -0700

    token validator middleware

---
+++ server/middleware/validate-token.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken')
+
+function getTokenFromHeaders(req) {
+    const token =
+        req.body.token || req.query.token || req.headers['master-token']
+
+    return token
+}
+
+function validateToken(req, res, next) {
+    const token = getTokenFromHeaders(req)
+
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+            if (err) {
+                return res.json({
+                    success: false,
+                    message: 'Failed to authenticate token'
+                })
+            } else {
+                req.decoded = decoded
+                next()
+            }
+        })
+    } else {
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided'
+        })
+    }
+}
+
+module.exports = validateToken
