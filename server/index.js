dda14a40501a7eb5e89d456e17869c9a90126d3a
William <wtpwinberg@gmail.com>
2020-03-06 17:05:48 -0700

    server init fixed

--- server/index.js
+++ server/index.js
@@ -3,51 +3,64 @@ const morgan = require('morgan')
 const app = express()
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
-const jwt = require('express-jwt')
+// const jwt = require('express-jwt')
 // const jsonwebtoken = require('jsonwebtoken')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const dbConfig = require('./config/index')
 
 // connect to mongodb
-mongoose
-    .connect(dbConfig.connection, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true
-    })
-    .then(() => console.log(`connection successful: ${dbConfig.connection}`))
-    .catch((err) => console.error(err))
+mongoose.connect(dbConfig.connection, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+// .then(() => console.log(`connection successful: ${dbConfig.connection}`))
+// .catch((err) => console.error(dbConfig.connection, err.message))
 
 app.use(cookieParser())
 app.use(bodyParser.json())
 
 // configure CORS settings
-const corsConfig = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
-    res.header('Access-Control-Allow-Credentials', true)
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    )
-    next()
-}
+// const corsConfig = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Credentials', true)
+//     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
+//     res.header(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//     )
+//     next()
+// }
 
-app.use(corsConfig)
+app.use(
+    cors({
+        allowedHeaders: ['sessionId', 'Content-Type', 'master-token'],
+        exposedHeaders: ['sessionId'],
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        preflightContinue: false
+    })
+)
+
 app.use(morgan('tiny'))
 
-// app.listen(3000)
+// app.use(corsConfig)
 
 // JWT middleware TODO: can this be moved to authRoutes.js?
 // app.use(
 //     jwt({
-//         secret: 'adumbsecret'
+//         secret: 'aprettydumbsecret'
 //     }).unless({
 //         path: ['/api/auth/login', '/login', '/']
 //     })
-// ) dd
+// )
 
-jwt({ secret: 'shhhhhhared-secret', audience: '/api/auth', issuer: '/' })
+// const token = jwt({
+//     secret: 'aprettydumbsecret'
+// audience: '/api/auth',
+// issuer: '/'
+// })
 
 const { router } = require('./routes')
 app.use(router)
